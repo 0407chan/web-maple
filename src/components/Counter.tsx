15714418fe5ee35e92685a91387123e9f5b5509d
이찬호 <0407chan@naver.com>
2020-09-09 15:18:32 +0900

    [#1] feat. draggable적용, drag boundery적용

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,7 +4,7 @@ function Counter() {
   const { count, onIncrease, onDecrease, onIncreaseBy } = useCounter();
   return (
     <div>
-      <h1>{count}</h1>
+      <div>{count}</div>
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
       <button onClick={() => onIncreaseBy(5)}>+5</button>

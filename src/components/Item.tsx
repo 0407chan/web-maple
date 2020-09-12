b3d4aaee1eb06ca070e1aabdaba891a6ff857a81
이찬호 <0407chan@naver.com>
2020-09-13 02:00:07 +0900

    [#4] feat. item에 hover 액션 추가

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,17 +1,47 @@
 import React from "react";
 import "./Item.scss";
 import { EquipType } from "../modules/inventory/types";
-
+import useToolTip from "../hooks/useToolTip";
+import useInventory from "../hooks/useInventory";
+import { emptyEquip } from "../dummy/equip";
 function Item(props: EquipType) {
+  const {
+    visible,
+    setDisplayNone,
+    setDisplayVisible,
+    onSetMousePosition,
+  } = useToolTip();
+  const { onSetCurrItem } = useInventory();
   const { id, image, name } = props;
 
   const preventDragHandler = (e: React.DragEvent<HTMLImageElement>) => {
     e.preventDefault();
   };
 
+  const setDispalyVisibleAction = () => {
+    onSetCurrItem(props);
+    setDisplayVisible();
+  };
+
+  const setDispalyNoneAction = () => {
+    // setDisplayNone();
+    // onSetCurrItem(emptyEquip);
+  };
+
+  const setMousePosition = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    onSetMousePosition(event.clientX, event.clientY);
+  };
+
   return (
     <>
-      <div className="item-wrapper">
+      <div
+        className="item-wrapper"
+        onMouseOver={setDispalyVisibleAction}
+        onMouseOut={setDispalyNoneAction}
+        onMouseMove={setMousePosition}
+      >
         <img
           src={image}
           className="item-img"

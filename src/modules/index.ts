6e7247dc5dcecb7881b6596871daed8a206a4378
이찬호 <0407chan@naver.com>
2020-09-13 01:58:11 +0900

    [#4] feat. rootReducer에 tooltip추가

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
 import inventory from "./inventory";
+import toolTip from "./toolTip";
 
 const rootReducer = combineReducers({
   counter,
   inventory,
+  toolTip,
 });
 
 export default rootReducer;

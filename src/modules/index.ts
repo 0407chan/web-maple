7d4de49921daa83e82482fd953d25493c0ed6e50
이찬호 <0407chan@naver.com>
2020-09-19 08:55:51 +0900

    [#4] feat. add item module

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import counter from "./counter";
 import inventory from "./inventory";
 import toolTip from "./toolTip";
+import item from "./item";
 
 const rootReducer = combineReducers({
   counter,
   inventory,
   toolTip,
+  item,
 });
 
 export default rootReducer;

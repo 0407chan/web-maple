e659abb3837df30cf5cf88cdcfee57eb0453c919
이찬호 <0407chan@naver.com>
2021-08-16 22:41:49 +0900

    [21] 인벤토리 커스텀 버튼 스타일 추가

--- src/components/Inventory/style.tsx
+++ src/components/Inventory/style.tsx
@@ -3,12 +3,12 @@ import styled from 'styled-components'
 export const Contianer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   background-color: rgba(0, 0, 0, 0.7);
   box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.8);
   border-radius: 5px;
   width: 300px;
   height: 500px;
-  cursor: pointer;
 `
 
 export const InventoryHeader = styled.div`
@@ -20,15 +20,58 @@ export const InventoryHeader = styled.div`
   color: #ffcc5f;
   padding: 10px;
 `
-export const ItemListWrapper = styled.div`
+export const InventoryBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  height: fit-content;
+  margin: 10px;
+  border-radius: 5px;
+  justify-content: center;
+  background-color: #eeeeeee7;
+`
+export const InventoryButtonWrapper = styled.div`
+  display: flex;
+  padding: 20px 10px 0px 10px;
+  flex-direction: row;
+  border-bottom: 2px solid #ffc156;
+`
+export const InventoryButton = styled.button`
+  display: flex;
+  border-top: 1px solid #9a722e;
+  border-right: 1px solid #9a722e;
+  border-left: 1px solid #9a722e;
+  border-bottom: unset;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  flex-direction: row;
+  font-weight: bold;
+  color: #fff8ed;
+  padding: 4px 10px;
+  cursor: pointer;
+  background-color: #a0a0a0;
+
+  &:hover {
+    background-color: #b5b5b5;
+  }
+
+  &.isActive {
+    background-color: #f4af38;
+
+    &:hover {
+      background-color: #ffc96b;
+    }
+  }
+`
+
+export const ItemWrapper = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   overflow-y: scroll;
-  height: 350px;
+  width: 250px;
+  height: 360px;
   gap: 10px;
-  margin: 10px;
   border-radius: 5px;
   padding: 10px;
-  background-color: #eeeeeee7;
 `

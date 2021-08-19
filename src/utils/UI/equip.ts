1d50f25a64c49d07a39fa8e2dc9aa5460abfcb7e
이찬호 <0407chan@naver.com>
2021-08-19 22:28:47 +0900

    [21] 이미지 lint 적용

--- src/utils/UI/equip.ts
+++ src/utils/UI/equip.ts
@@ -1,11 +1,11 @@
-import equip1 from "../../image/item/etc/04000001.info.icon.png";
-import equip2 from "../../image/item/etc/04000002.info.icon.png";
-import equip3 from "../../image/item/etc/04000003.info.icon.png";
-import equip4 from "../../image/item/etc/04000004.info.icon.png";
-import equip5 from "../../image/item/etc/04000005.info.icon.png";
-import equip6 from "../../image/item/etc/04000006.info.icon.png";
-import equip7 from "../../image/item/etc/04000007.info.icon.png";
-import equip8 from "../../image/item/etc/04000008.info.icon.png";
+import equip1 from '../../image/item/etc/04000001.info.icon.png'
+import equip2 from '../../image/item/etc/04000002.info.icon.png'
+import equip3 from '../../image/item/etc/04000003.info.icon.png'
+import equip4 from '../../image/item/etc/04000004.info.icon.png'
+import equip5 from '../../image/item/etc/04000005.info.icon.png'
+import equip6 from '../../image/item/etc/04000006.info.icon.png'
+import equip7 from '../../image/item/etc/04000007.info.icon.png'
+import equip8 from '../../image/item/etc/04000008.info.icon.png'
 
 export const EquipImages = [
   equip1,
@@ -15,5 +15,5 @@ export const EquipImages = [
   equip5,
   equip6,
   equip7,
-  equip8,
-];
+  equip8
+]

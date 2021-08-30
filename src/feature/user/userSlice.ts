d33f6abf7c477cb20423497fcf14535bfe69e874
이찬호 <0407chan@naver.com>
2021-08-30 21:45:30 +0900

    [43] userType에서 변경으로 인한 코드 제거

--- src/feature/user/userSlice.ts
+++ src/feature/user/userSlice.ts
@@ -9,51 +9,35 @@ const initUser: UserType = {
   userId: 1,
   STR: {
     base: 0,
-    bonus: 0,
-    label: 'STR',
-    reinforce: 0
+    bonus: 0
   },
   DEX: {
     base: 0,
-    bonus: 0,
-    label: 'DEX',
-    reinforce: 0
+    bonus: 0
   },
   INT: {
     base: 0,
-    bonus: 0,
-    label: 'INT',
-    reinforce: 0
+    bonus: 0
   },
   LUK: {
     base: 0,
-    bonus: 0,
-    label: 'LUK',
-    reinforce: 0
+    bonus: 0
   },
   HP: {
     base: 0,
-    bonus: 0,
-    label: 'HP',
-    reinforce: 0
+    bonus: 0
   },
   MP: {
     base: 0,
-    bonus: 0,
-    label: 'MP',
-    reinforce: 0
+    bonus: 0
   },
   WEAPON_ATTACK: {
     base: 0,
-    bonus: 0,
-    label: 'WEAPON_ATTACK',
-    reinforce: 0
+    bonus: 0
   },
   MAGIC_ATTACK: {
     base: 0,
-    bonus: 0,
-    label: 'MAGIC_ATTACK',
-    reinforce: 0
+    bonus: 0
   }
 }
 const initialState: UserState = {

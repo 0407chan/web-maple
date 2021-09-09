7fc69da07752bf8df4b377f7cfbd3a60ac4d798b
이찬호 <0407chan@naver.com>
2021-09-09 23:31:02 +0900

    [49] isOpenedWindow hook 추가

--- src/hooks/useUiWindow.ts
+++ src/hooks/useUiWindow.ts
@@ -2,7 +2,7 @@ import { addUiWindow, removeUiWindow } from '@/feature/uiWindow/uiWindowSlice'
 import { RootState } from '@/store'
 import { useDispatch, useSelector } from 'react-redux'
 
-export default function useUiWindow() {
+const useUiWindow = () => {
   const dispatch = useDispatch()
   const uiWindowList = useSelector(
     (state: RootState) => state.uiWindow.uiWindowList
@@ -10,19 +10,28 @@ export default function useUiWindow() {
 
   const onRemoveUiWindow = (windowName: string) =>
     dispatch(removeUiWindow({ windowName }))
-  const onAddUiWindow = (windowName: string) =>
+
+  const onAddUiWindow = (windowName: string) => {
     dispatch(addUiWindow({ windowName }))
+  }
+
   const onRemoveLastWindow = () => {
     const lastWindow = Array.from(uiWindowList).pop()
     if (lastWindow) {
       dispatch(removeUiWindow({ windowName: lastWindow }))
     }
   }
+  const isOpenedWindow = (windowName: string) => {
+    return uiWindowList.find((value) => value === windowName)
+  }
 
   return {
     uiWindowList,
     onRemoveUiWindow,
     onAddUiWindow,
-    onRemoveLastWindow
+    onRemoveLastWindow,
+    isOpenedWindow
   }
 }
+
+export default useUiWindow

6a895ac274372cc65407b36f6eb807e860762318
이찬호 <0407chan@naver.com>
2021-09-09 22:21:28 +0900

    [49] uiWinodwSlice 생성, removeUiwindow 함수 생성

---
+++ src/feature/uiWindow/uiWindowSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export type UiWindowState = {
+  uiWindowList: Set<string>
+}
+
+export const initialState: UiWindowState = {
+  uiWindowList: new Set()
+}
+
+export const uiWindowSlice = createSlice({
+  name: 'uiWindow',
+  initialState,
+  reducers: {
+    removeUiWindow: (state, action: PayloadAction<{ windowName: string }>) => {
+      const result = new Set(state.uiWindowList)
+      result.delete(action.payload.windowName)
+      state.uiWindowList = result
+    }
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const { removeUiWindow } = uiWindowSlice.actions
+
+export default uiWindowSlice.reducer

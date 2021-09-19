f8f0214e356259f59f9bc5d5a605b5d066cdabf2
이찬호 <0407chan@naver.com>
2021-09-19 20:25:28 +0900

    [51] equipment api 파일 생성. item 호출 코드 작성

---
+++ src/api/equipment.ts
@@ -0,0 +1,25 @@
+import { GetEquipmentQuery, GetEquipmentResponse } from '@/types/equipment'
+import axios, { AxiosResponse } from 'axios'
+import { useQuery, UseQueryResult } from 'react-query'
+
+export const getEquipment = async (
+  query?: GetEquipmentQuery
+): Promise<AxiosResponse<GetEquipmentResponse>> => {
+  const result = await axios.get(
+    'https://maplestory.io/api/KMS/352/item/1302000',
+    { params: query }
+  )
+  return result.data
+}
+
+export const useGetEquipment = (
+  query?: GetEquipmentQuery
+): UseQueryResult<GetEquipmentResponse, unknown> => {
+  return useQuery(
+    ['getEquipment', query],
+    async () => {
+      return getEquipment(query)
+    },
+    { retry: false, refetchOnWindowFocus: false, keepPreviousData: true }
+  )
+}

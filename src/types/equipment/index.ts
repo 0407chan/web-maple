1f9c7f00ff0fbff47d965ee371f1d20c15aab53c
이찬호 <0407chan@naver.com>
2021-09-19 20:25:45 +0900

    [51] maple api의 response 타입 정의

--- src/types/equipment/index.ts
+++ src/types/equipment/index.ts
@@ -15,3 +15,127 @@ export type EquipSlotCategory =
   | 'EYE'
   | 'RING'
   | 'PENDANT'
+
+export type GetEquipmentQuery = {
+  startPosition?: number
+  count?: number
+  overallCategoryFilter?: string
+  categoryFilter?: string
+  subCategoryFilter?: string
+  jobFilter?: number
+  cashFilter?: boolean
+  minLevelFilter?: number
+  maxLevelFilter?: number
+  genderFilter?: number
+  searchFor?: string
+}
+export type GetEquipmentResponse = EquipmentItemType
+
+export type Origin = {
+  hasValue: boolean
+  value: Position
+}
+
+export type Description = {
+  id: number
+  name: string
+  description: string
+}
+
+export type IconOrigin = {
+  hasValue: boolean
+  value: Position
+}
+
+export type IconRawOrigin = {
+  hasValue: boolean
+  value: Position
+}
+
+export type MetaInfo = {
+  only: boolean
+  cash: boolean
+  reqLevel: number
+  mob: number
+  iconRaw: string
+  icon: string
+  iconOrigin: IconOrigin
+  iconRawOrigin: IconRawOrigin
+  slotMax: number
+  price: number
+  reqSTR: number
+  reqDEX: number
+  reqINT: number
+  reqLUK: number
+  reqJob: number
+  reqLevelEquip: number
+  tuc: number
+  incPAD: number
+  attack: number
+  attackSpeed: number
+  vslots: string[]
+  islots: string[]
+  setCompleteCount: number
+}
+
+export type TypeInfo = {
+  overallCategory: string
+  category: string
+  subCategory: string
+  lowItemId: number
+  highItemId: number
+}
+
+export type EquipmentItemType = {
+  frameBooks: FrameBooks
+  equipGroup: string
+  id: number
+  description: Description
+  metaInfo: MetaInfo
+  typeInfo: TypeInfo
+}
+
+export type FrameBooks = {
+  alert: Frame[]
+  fly: Frame[]
+  heal: Frame[]
+  jump: Frame[]
+  prone: Frame[]
+  proneStab: Frame[]
+  stabO1: Frame[]
+  stabO2: Frame[]
+  stabOF: Frame[]
+  stand1: Frame[]
+  swingO1: Frame[]
+  swingO2: Frame[]
+  swingO3: Frame[]
+  swingOF: Frame[]
+  walk1: Frame[]
+}
+
+export type Weapon = {
+  image: string
+  origin: {
+    hasValue: boolean
+    value: Position
+  }
+  originOrZero: Position
+  mapOffset: {
+    hand: Position
+  }
+  position: string
+}
+
+export type Effects = {
+  weapon: Weapon
+}
+
+export type Frame = {
+  effects: Effects
+}
+
+type Position = {
+  x: number
+  y: number
+  isEmpty: boolean
+}

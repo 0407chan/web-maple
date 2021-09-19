b16cd306c1991fb71b87f1c817701060561088fc
이찬호 <0407chan@naver.com>
2021-09-19 20:25:04 +0900

    [51] axios 설치

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './store'
 import GlobalStyle from './style/GlobalStyle'
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <Provider store={store}>
     <DndProvider backend={HTML5Backend}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
       <GlobalStyle />
     </DndProvider>
   </Provider>,

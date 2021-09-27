a2e54faee91971e35625228fc899367b32aae917
이찬호 <0407chan@naver.com>
2021-09-27 20:32:25 +0900

    [54] 단일스텟, 이중스텟 계산 함수 추가

--- src/components/FlameOfResurrection/utils.ts
+++ src/components/FlameOfResurrection/utils.ts
@@ -1,3 +1,37 @@
+import { EquipItemType } from '@/types/inventory'
+import {
+  ETERNAL_FLAME_PERCENTAGE,
+  POWERFUL_FLAME_PERCENTAGE
+} from './constants'
+
 export const getRandomNum = (range: number): number => {
   return Math.floor(Math.random() * range + 1)
+}
+
+export const calcSingleBonusStatEternal = (item: EquipItemType): number => {
+  const grade = ETERNAL_FLAME_PERCENTAGE.get(getRandomNum(100))
+  const bossGrade = item.bossReward ? 2 : 0
+  if (grade === undefined) return 0
+  return (Math.floor(item.level / 20) + 1) * (bossGrade + grade)
+}
+export const calcDoubleBonusStatEternal = (item: EquipItemType): number => {
+  const grade = ETERNAL_FLAME_PERCENTAGE.get(getRandomNum(100))
+  const bossGrade = item.bossReward ? 2 : 0
+  if (grade === undefined) return 0
+  return (Math.floor(item.level / 40) + 1) * (bossGrade + grade)
+}
+
+// 단일 스텟
+export const calcSingleBonusStatPowerful = (item: EquipItemType): number => {
+  const grade = POWERFUL_FLAME_PERCENTAGE.get(getRandomNum(100))
+  const bossGrade = item.bossReward ? 2 : 0
+  if (grade === undefined) return 0
+  return (Math.floor(item.level / 20) + 1) * (bossGrade + grade)
+}
+// 이중 스텟
+export const calcDoubleBonusStatPowerful = (item: EquipItemType): number => {
+  const grade = ETERNAL_FLAME_PERCENTAGE.get(getRandomNum(100))
+  const bossGrade = item.bossReward ? 2 : 0
+  if (grade === undefined) return 0
+  return (Math.floor(item.level / 40) + 1) * (bossGrade + grade)
 }

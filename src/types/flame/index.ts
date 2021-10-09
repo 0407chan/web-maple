e7471efa7645ab370946353ec9786de32d48a1f9
이찬호 <0407chan@naver.com>
2021-10-09 17:36:44 +0900

    [59] 환불 타입 추가

---
+++ src/types/flame/index.ts
@@ -0,0 +1,11 @@
+export type StatusSettingType = {
+  STR?: number
+  DEX?: number
+  INT?: number
+  LUK?: number
+  AllStat?: number
+  WEAPON_ATTACK?: number
+  MAGIC_ATTACK?: number
+  bossDemage?: number
+  demage?: number
+}

75f21eeeded115755ed66f2a6b41ffd2d5e0ef5a
이찬호 <0407chan@naver.com>
2021-10-20 23:57:20 +0900

    [63] inventorySection에 position 정해주기

--- src/components/EquipmentStore/EquipmentStore.tsx
+++ src/components/EquipmentStore/EquipmentStore.tsx
@@ -2,8 +2,10 @@ import useItem from '@/hooks/useItem'
 import { EquipmentItemListType } from '@/types/equipment'
 import Input from 'antd/lib/input'
 import React, { useEffect, useRef, useState } from 'react'
+import { ControlPosition, DraggableData, DraggableEvent } from 'react-draggable'
 import MapleButton from '../common/MapleButton'
 import WindowContainer from '../common/WindowContainer'
+import InventorySection from './InventorySection'
 import StoreSlot from './StoreSlot'
 import * as S from './style'
 
@@ -18,6 +20,10 @@ const EquipmentStore: React.FC<EquipmentStoreProps> = () => {
     useState<EquipmentItemListType[]>(equipmentItemList)
   const [maxCount, setMaxCount] = useState<number>(30)
   const ref = useRef<HTMLDivElement>(null)
+  const [position, setPosition] = useState<ControlPosition>({
+    x: document.body.clientWidth / 2 - 350,
+    y: 200
+  })
 
   useEffect(() => {
     const newArray = equipmentItemList.filter(
@@ -26,43 +32,52 @@ const EquipmentStore: React.FC<EquipmentStoreProps> = () => {
     setSearchedList(newArray)
   }, [searchKey])
 
+  const onControlledDrag = (e: DraggableEvent, data: DraggableData) => {
+    const { x, y } = data
+    setPosition({ x, y })
+  }
+
   return (
-    <WindowContainer
-      title="EQUIPMENT STORE"
-      canDrag={false}
-      hideCloseButton
-      windowType="EquipmentStore"
-      style={{ left: document.body.clientWidth / 2 - 150, top: 200 }}
-    >
-      <S.Vertical>
-        <Input
-          placeholder="구매할 아이템 이름을 입력해주세요."
-          value={searchKey}
-          onChange={(event) => {
-            event.nativeEvent.cancelBubble = true
-            setSearchKey(event.target.value)
-          }}
-        />
-        <S.Contianer ref={ref}>
-          {searchedList.slice(0, maxCount).map((item) => (
-            <StoreSlot key={item.id} item={item} searchKey={searchKey} />
-          ))}
-          {searchedList.length > maxCount && (
-            <MapleButton
-              style={{ margin: 10 }}
-              onClick={() => setMaxCount(maxCount + 30)}
-            >
-              더 보기
-            </MapleButton>
-          )}
-          {searchedList.length === 0 && (
-            <S.EmptyContainer>
-              [<S.BoldText>{searchKey}</S.BoldText>] 는 존재하지 않습니다.
-            </S.EmptyContainer>
-          )}
-        </S.Contianer>
-      </S.Vertical>
-    </WindowContainer>
+    <>
+      <WindowContainer
+        title="EQUIPMENT STORE"
+        canDrag={false}
+        onDrag={onControlledDrag}
+        hideCloseButton
+        windowType="EquipmentStore"
+        position={position}
+      >
+        <S.Vertical>
+          <Input
+            placeholder="구매할 아이템 이름을 입력해주세요."
+            value={searchKey}
+            onChange={(event) => {
+              event.nativeEvent.cancelBubble = true
+              setSearchKey(event.target.value)
+            }}
+          />
+          <S.Contianer ref={ref}>
+            {searchedList.slice(0, maxCount).map((item) => (
+              <StoreSlot key={item.id} item={item} searchKey={searchKey} />
+            ))}
+            {searchedList.length > maxCount && (
+              <MapleButton
+                style={{ margin: 10 }}
+                onClick={() => setMaxCount(maxCount + 30)}
+              >
+                더 보기
+              </MapleButton>
+            )}
+            {searchedList.length === 0 && (
+              <S.EmptyContainer>
+                [<S.BoldText>{searchKey}</S.BoldText>] 는 존재하지 않습니다.
+              </S.EmptyContainer>
+            )}
+          </S.Contianer>
+        </S.Vertical>
+      </WindowContainer>
+      <InventorySection position={position} />
+    </>
   )
 }
 

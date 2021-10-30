d585347bf32a8ffc57b0c4d81669db5263a0c917
이찬호 <0407chan@naver.com>
2021-10-31 00:40:00 +0900

    [76] 슈페리얼 별 개수 구하는 함수 생성

--- src/components/StartForce/constants.ts
+++ src/components/StartForce/constants.ts
@@ -11,3 +11,16 @@ export const getMaxStar = (level: number): number => {
     return 20
   } else return 25
 }
+export const getMaxStarForSuperior = (level: number): number => {
+  if (level <= 94) {
+    return 3
+  } else if (level >= 95 && level <= 107) {
+    return 5
+  } else if (level >= 108 && level <= 117) {
+    return 8
+  } else if (level >= 118 && level <= 127) {
+    return 10
+  } else if (level >= 128 && level <= 137) {
+    return 12
+  } else return 15
+}

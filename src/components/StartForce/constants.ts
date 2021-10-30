35279e6ea3ed98452931435ac537d51ce24ea4e2
이찬호 <0407chan@naver.com>
2021-10-30 23:43:38 +0900

    [74] getMaxStar에 아이템 말고 level만 받아서 리턴하도록 변경

--- src/components/StartForce/constants.ts
+++ src/components/StartForce/constants.ts
@@ -1,15 +1,13 @@
-import { EquipItemType } from '@/types/inventory'
-
-export const getMaxStar = (item: EquipItemType): number => {
-  if (item.level <= 94) {
+export const getMaxStar = (level: number): number => {
+  if (level <= 94) {
     return 5
-  } else if (item.level >= 95 && item.level <= 107) {
+  } else if (level >= 95 && level <= 107) {
     return 8
-  } else if (item.level >= 108 && item.level <= 117) {
+  } else if (level >= 108 && level <= 117) {
     return 15
-  } else if (item.level >= 118 && item.level <= 127) {
+  } else if (level >= 118 && level <= 127) {
     return 15
-  } else if (item.level >= 128 && item.level <= 137) {
+  } else if (level >= 128 && level <= 137) {
     return 20
   } else return 25
 }

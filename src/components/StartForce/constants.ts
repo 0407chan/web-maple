d82b19b25814f0830e2bbd11357dda737403cad9
이찬호 <0407chan@naver.com>
2021-10-31 00:28:00 +0900

    [74] maxStar 수치 잘못입력한 오류 수정

--- src/components/StartForce/constants.ts
+++ src/components/StartForce/constants.ts
@@ -4,7 +4,7 @@ export const getMaxStar = (level: number): number => {
   } else if (level >= 95 && level <= 107) {
     return 8
   } else if (level >= 108 && level <= 117) {
-    return 15
+    return 10
   } else if (level >= 118 && level <= 127) {
     return 15
   } else if (level >= 128 && level <= 137) {

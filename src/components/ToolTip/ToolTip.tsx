84db2bfd093e239dbba0accb36841b56c57abccd
이찬호 <0407chan@naver.com>
2021-11-01 22:58:46 +0900

    툴팁에 star에서 파란 별 제거

--- src/components/ToolTip/ToolTip.tsx
+++ src/components/ToolTip/ToolTip.tsx
@@ -163,13 +163,15 @@ const ToolTip: React.FC<ToolTipProps> = ({ positionX, positionY }) => {
 
   const renderStar = () => {
     const result = []
-    console.log(currentItem.name, currentItem.maxStar)
     for (let i = 0; i < maxStar; i++) {
-      const starImage = currentItem.isSuperior
-        ? IMAGE.tooltip.tooltip_Item_Star_blue
-        : IMAGE.tooltip.tooltip_Item_Star
+      // const starImage =
+      // currentItem.isSuperior
+      //   ? IMAGE.tooltip.tooltip_Item_Star_blue
+      //   : IMAGE.tooltip.tooltip_Item_Star
       const imageSrc =
-        i < star ? starImage : IMAGE.tooltip.tooltip_Item_Star_none
+        i < star
+          ? IMAGE.tooltip.tooltip_Item_Star
+          : IMAGE.tooltip.tooltip_Item_Star_none
       result.push(
         <img src={imageSrc} key={i} width={13} alt={`starimg-` + i} />
       )

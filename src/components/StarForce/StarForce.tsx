1f4dd4cfd2f201ea86ca781aa58df75bffc07c07
이찬호 <0407chan@naver.com>
2021-11-02 23:48:10 +0900

    [76] 스타포스 창 기본 레이아웃 잡기

--- src/components/StarForce/StarForce.tsx
+++ src/components/StarForce/StarForce.tsx
@@ -1,10 +1,15 @@
 import useEquipment from '@/hooks/useEquipment'
 import useInventory from '@/hooks/useInventory'
+import useToolTip from '@/hooks/useToolTip'
 import { EquipItemType, SlotType } from '@/types/inventory'
+import IMAGE from '@/utils/images'
 import React, { useState } from 'react'
 import { ControlPosition } from 'react-draggable'
 import { v4 as uuid } from 'uuid'
+import MapleButton from '../common/MapleButton'
 import WindowContainer from '../common/WindowContainer'
+import Slot from '../Inventory/Slot'
+import * as S from './style'
 
 const initSlot: SlotType = {
   id: uuid(),
@@ -12,13 +17,37 @@ const initSlot: SlotType = {
 }
 
 const StarForce: React.FC = () => {
+  let clickTimer: any = undefined
   const { inventory, currentInventory, onUpdateInventorySlot } = useInventory()
   const { equipment, onUpdateEquipSlot } = useEquipment()
+  const { onHideTooltip } = useToolTip()
+  const [showResult, setShowResult] = useState<boolean>(true)
+  const [showSetting, setShowSetting] = useState<boolean>(true)
   const [position, setPosition] = useState<ControlPosition>({
     x: document.body.clientWidth / 2 - 150,
     y: 200
   })
   const [starForceSlot, setStarForceSlot] = useState<SlotType>(initSlot)
+
+  const onDrop = (slot: any) => {
+    setStarForceSlot({ ...slot })
+  }
+  const isMySlot = (item: SlotType) => {
+    return true
+  }
+  const onItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    clearTimeout(clickTimer)
+    if (event.detail === 1) {
+      clickTimer = setTimeout(() => {
+        console.log('싱글 클릭')
+      }, 200)
+    } else if (event.detail === 2) {
+      if (starForceSlot.item) {
+        setStarForceSlot(initSlot)
+        onHideTooltip()
+      }
+    }
+  }
 
   const setItemOnOriginalSlot = (newItem: EquipItemType) => {
     const equipSlot = equipment.find((slot) => slot.item?.id === newItem.id)
@@ -35,6 +64,10 @@ const StarForce: React.FC = () => {
     }
   }
 
+  const onStarForce = () => {
+    console.log('가즈아')
+  }
+
   return (
     <WindowContainer
       title="EQUIPMENT ENCHANT"
@@ -42,7 +75,65 @@ const StarForce: React.FC = () => {
       onDrag={(e, data) => setPosition({ x: data.x, y: data.y })}
       position={position}
     >
-      StarForce
+      <S.Contianer>
+        <Slot
+          slot={starForceSlot}
+          onDrop={onDrop}
+          isMySlot={isMySlot}
+          onClick={onItemClick}
+          className="star-force-slot"
+          isCanDrop={false}
+        />
+        <S.Result>
+          <S.Horizontal>
+            {starForceSlot.item &&
+              `${starForceSlot.item.star}성 > ${starForceSlot.item.star + 1}성`}
+          </S.Horizontal>
+          <S.Horizontal style={{ justifyContent: 'space-around' }}>
+            <S.Vertical>
+              <div>90%</div>
+              <div>성공</div>
+            </S.Vertical>
+            <S.Vertical>
+              <div>8%</div>
+              <div>실패</div>
+            </S.Vertical>
+            <S.Vertical>
+              <div>2%</div>
+              <div>파괴</div>
+            </S.Vertical>
+          </S.Horizontal>
+        </S.Result>
+        <S.Horizontal>
+          <MapleButton onClick={() => setShowSetting(!showSetting)}>
+            세팅
+          </MapleButton>
+          {/* <Checkbox
+              // checked={isAuto}
+              onChange={(event) => {
+                if (event.target.checked === false && intervalRef.current) {
+                  clearInterval(intervalRef.current)
+                }
+                setIsAuto(event.target.checked)
+              }}
+            >
+              <S.Title>자동</S.Title>
+            </Checkbox> */}
+        </S.Horizontal>
+        <S.Horizontal>
+          <MapleButton
+            disabled={starForceSlot.item === undefined}
+            onClick={onStarForce}
+          >
+            <img
+              width={13}
+              src={IMAGE.tooltip.tooltip_Item_Star}
+              alt="star-force-image"
+            />
+            강화
+          </MapleButton>
+        </S.Horizontal>
+      </S.Contianer>
     </WindowContainer>
   )
 }
--- src/components/StarForce/style.ts
+++ src/components/StarForce/style.ts
@@ -2,4 +2,40 @@ import styled from 'styled-components'
 
 export const Contianer = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  border-radius: 5px;
+  gap: 10px;
+`
+
+export const Result = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  height: 190px;
+  width: 270px;
+  align-items: flex-start;
+  justify-content: flex-start;
+  background-color: rgba(120, 120, 120, 0.95);
+  border-radius: 5px;
+  padding: 10px;
+  color: #e1e2e3;
+`
+
+export const Horizontal = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 5px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Vertical = styled.div`
+  display: flex;
+  gap: 5px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `

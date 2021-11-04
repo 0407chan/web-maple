4e0f9ebd53311012cb9eaf694c19700005b4e6a5
이찬호 <0407chan@naver.com>
2021-11-04 21:17:41 +0900

    [76] SuccessRate 타입 fail타입 변경

--- src/types/star-force/index.ts
+++ src/types/star-force/index.ts
@@ -1,7 +1,6 @@
 export type SuccessRate = {
   success: number
-  fail: number
-  maintain?: number
-  decrease?: number
-  destroy?: number
+  failMaintain: number
+  failDecrease: number
+  destroy: number
 }

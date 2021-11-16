9245cb5c0f3623df7bf5052927242fe358bcc6ab
이찬호 <0407chan@naver.com>
2021-11-16 23:50:40 +0900

    [83] 5,10,15 이벤트 함수 추가

--- src/components/StarForce/utils.ts
+++ src/components/StarForce/utils.ts
@@ -15,3 +15,18 @@ export const isOnePlusOne = (
 
   return false
 }
+
+export const is1516 = (
+  item: EquipItemType,
+  starForceSetting: StarForceSetting
+): boolean => {
+  if (
+    starForceSetting.event1516 === true &&
+    item.isSuperior !== true &&
+    (item.star === 5 || item.star === 10 || item.star === 15)
+  ) {
+    return true
+  }
+
+  return false
+}

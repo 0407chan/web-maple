344262bf821e5c21b3a41bc67da9f3b5e9324650
이찬호 <0407chan@naver.com>
2021-11-16 22:57:51 +0900

    chore. 이미지 util 위치변경

--- src/utils/images.ts
+++
@@ -1,9 +0,0 @@
-import { EquipImages } from "./UI/equip";
-import { images } from "./UI/inventory";
-import { tooltip } from "./UI/tooltip";
-
-export default {
-  EquipImages,
-  images,
-  tooltip,
-};
---
+++ src/utils/images/index.ts
@@ -0,0 +1,9 @@
+import { EquipImages } from '../UI/equip'
+import { images } from '../UI/inventory'
+import { tooltip } from '../UI/tooltip'
+
+export default {
+  EquipImages,
+  images,
+  tooltip
+}

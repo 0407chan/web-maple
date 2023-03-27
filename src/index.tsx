0d8f08c99e3df30832f749982a73ed3334f825c1
이찬호 <0407chan@naver.com>
2023-03-27 23:58:03 +0900

    redux persist 반영, hooks추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,31 @@
-import React from 'react'
 import { DndProvider } from 'react-dnd-multi-backend'
 import HTML5toTouch from 'react-dnd-multi-backend/dist/esm/HTML5toTouch'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App'
-import { store } from './store'
+
+import { store } from './redux/store'
 import './style/custom-style.scss'
 import GlobalStyle from './style/GlobalStyle'
 
 const queryClient = new QueryClient()
 
-ReactDOM.render(
+const persistor = persistStore(store)
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <Provider store={store}>
-    <DndProvider options={HTML5toTouch}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-      <GlobalStyle />
-    </DndProvider>
-  </Provider>,
-  document.getElementById('root')
+    <PersistGate loading={null} persistor={persistor}>
+      <DndProvider options={HTML5toTouch}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+        <GlobalStyle />
+      </DndProvider>
+    </PersistGate>
+  </Provider>
 )
---
+++ src/redux/store.ts
@@ -0,0 +1,52 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import counterReducer from 'feature/counter/counterSlice'
+import equipmentReducer from 'feature/equipment/equipmentSlice'
+import inventoryReducer from 'feature/inventory/inventorySlice'
+import itemReducer from 'feature/item/itemSlice'
+import tooltipReducer from 'feature/tooltip/tooltipSlice'
+import uiWindowReducer from 'feature/uiWindow/uiWindowSlice'
+import userReducer from 'feature/user/userSlice'
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['inventory']
+}
+
+const rootReducer = combineReducers({
+  counterReducer,
+  inventoryReducer,
+  tooltipReducer,
+  userReducer,
+  uiWindowReducer,
+  equipmentReducer,
+  itemReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+  }
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
